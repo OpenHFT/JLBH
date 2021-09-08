a2e49d4081671797141cd745537f3cdcf47428cd
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-09-08 15:11:46 +0100

    ignored for mac

--- src/test/java/net/openhft/chronicle/jlbh/JLBHTest.java
+++ src/test/java/net/openhft/chronicle/jlbh/JLBHTest.java
@@ -1,7 +1,9 @@
 package net.openhft.chronicle.jlbh;
 
+import net.openhft.chronicle.core.OS;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -21,6 +23,8 @@ public class JLBHTest {
 
     @Test
     public void shouldWriteResultToTheOutputProvided() {
+        Assume.assumeTrue(!OS.isMacOSX());
+
         // given
         final OutputStream outputStream = new ByteArrayOutputStream();
         final JLBH jlbh = new JLBH(options(), new PrintStream(outputStream), resultConsumer());
@@ -42,6 +46,7 @@ public class JLBHTest {
      * and JLBHDeterministicFixtures::expectedOutput in particular
      */
     public void shouldProvideResultData() {
+        Assume.assumeTrue(!OS.isMacOSX());
         // given
         final JLBHResultConsumer resultConsumer = resultConsumer();
         final JLBH jlbh = new JLBH(options(), printStream(), resultConsumer);
@@ -96,6 +101,7 @@ public class JLBHTest {
 
     @Test
     public void shouldProvideResultDataEvenIfProbesDoNotProvideSameShapedData() {
+        Assume.assumeTrue(!OS.isMacOSX());
         // given
         final JLBHResultConsumer resultConsumer = resultConsumer();
         JLBHOptions jlbhOptions = options().jlbhTask(new PredictableJLBHTaskDifferentShape()).iterations(ITERATIONS * 2);
@@ -114,6 +120,7 @@ public class JLBHTest {
 
     @Test
     public void teamCityHelper() {
+        Assume.assumeTrue(!OS.isMacOSX());
         // given
         final JLBHResultConsumer resultConsumer = resultConsumer();
         JLBHOptions jlbhOptions = options().jlbhTask(new PredictableJLBHTaskDifferentShape()).iterations(ITERATIONS * 2);
@@ -145,6 +152,7 @@ public class JLBHTest {
 
     @Test
     public void shouldCallAllLifecycleMethods() {
+        Assume.assumeTrue(!OS.isMacOSX());
         AtomicInteger initCount = new AtomicInteger(0);
         AtomicInteger runCount = new AtomicInteger(0);
 
