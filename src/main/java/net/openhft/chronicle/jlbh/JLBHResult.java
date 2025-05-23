373607a14d12eb9f41517dc7378b2cf678efd75c
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-23 21:28:06 +0100

    Add return Javadoc for summaryOfLastRun

--- src/main/java/net/openhft/chronicle/jlbh/JLBHResult.java
+++ src/main/java/net/openhft/chronicle/jlbh/JLBHResult.java
@@ -41,6 +41,11 @@ public interface JLBHResult {
 
     interface ProbeResult {
 
+        /**
+         * Obtain the summary statistics for the most recent run of this probe.
+         *
+         * @return summary of the last run
+         */
         @NotNull
         RunResult summaryOfLastRun();
 
