2aab0642a70f843cab64204ad89ab5c4fcbdc6bf
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-24 06:34:21 +0100

    Add Javadoc for ImmutableProbeResult constructor

--- src/main/java/net/openhft/chronicle/jlbh/ImmutableProbeResult.java
+++ src/main/java/net/openhft/chronicle/jlbh/ImmutableProbeResult.java
@@ -30,6 +30,17 @@ final class ImmutableProbeResult implements JLBHResult.ProbeResult {
     @NotNull
     private final List<JLBHResult.RunResult> runsSummary;
 
+    /**
+     * Construct a probe result from the percentile values collected during each
+     * benchmark run.
+     *
+     * <p>The {@code percentileRuns} list should contain one array for every run
+     * of the benchmark. Each array is expected to be the values returned by a
+     * {@link net.openhft.chronicle.core.util.Histogram#getPercentiles()} call,
+     * i.e. latency percentiles in nanoseconds.</p>
+     *
+     * @param percentileRuns list of percentile arrays for each run
+     */
     public ImmutableProbeResult(List<double[]> percentileRuns) {
         runsSummary = unmodifiableList(percentileRuns.stream().map(ImmutableRunResult::new).collect(toList()));
     }
